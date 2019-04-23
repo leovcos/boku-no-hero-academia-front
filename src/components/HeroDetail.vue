06e5033df0f53667e6a98f052da215b142a5d74c
Filipe Meneses <flcmeneses@gmail.com>
2019-04-23 10:19:40 -0300

    Improving hero detail page

--- src/components/HeroDetail.vue
+++ src/components/HeroDetail.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="HeroDetail">
         <div class="HeroDetail__background">
-            <div class="HeroDetail__image" :style="{'background-image': `url(${hero.image})`}">
+            <div class="HeroDetail__image">
+                <img :src="hero.image"/>
             </div>
         </div>
         <div class="HeroDetail__wrapper">
             <div class="HeroDetail__name">
                 {{hero.name}}
+            </div>
+            <div class="HeroDetail__birthday">
+                {{hero.birthday | date}}
+            </div>
+            <div class="HeroDetail__quirk">
+                {{hero.quirk.name}}
             </div>
         </div>
     </div>
@@ -19,6 +26,15 @@ export default {
     hero: {
       type: Object,
       required: true
+    }
+  },
+  filters: {
+    date (val) {
+      if (!val) {
+        return ''
+      }
+      val = new Date(val)
+      return `${val.getDate()}/${val.getMonth() + 1}/${val.getFullYear()}`
     }
   }
 }
--- src/views/HeroDetail.vue
+++ src/views/HeroDetail.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="page-hero-detail">
+        <nav class="page-hero-detail__nav">
+          <a class="button is-primary" @click="back">Voltar</a>
+        </nav>
         <HeroDetail :hero="hero"/>
     </div>
 </template>
@@ -22,6 +25,9 @@ export default {
   },
   methods: {
     ...mapActions(['requestHero']),
+    back () {
+      this.$router.go(-1)
+    },
     async init () {
       this.hero = await this.requestHero({ id: this.$route.params.id })
       console.log(this.hero)
